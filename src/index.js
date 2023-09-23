80136d167d3d8a787263460969ffa406ef91396c
its-me-bauddha <harshbauddha563@gmail.com>
2023-09-23 12:25:28 +0530

    Added transport object and tried

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const { ServerConfig,Logger } = require("./config");
 const apiRoutes = require("./routes");
+
+const mailsender = require('./config/email-config')
+
 const app = express();
 
 app.use('/api' , apiRoutes);
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT,async () => {
   console.log(`Listening.... at port  :${ServerConfig.PORT}`);
-  Logger.info("Successfully started the server",{});
+   try {
+    const response = await mailsender.sendMail({
+      from:ServerConfig.GMAIL_EMAIL,
+      to:'h***8@gmail.com',
+      subject:'Is the service working ?',
+      text:'Yes it is working'
+    });
+    console.log(response);
+   } catch (error) {
+    console.log(error); 
+   }
 });
